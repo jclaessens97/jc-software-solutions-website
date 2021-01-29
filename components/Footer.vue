f34f1abdda7cf27fa7d536e6413d6eb9a6852c66
Jeroen Claessens <jclaessens97@hotmail.com>
2021-01-29 17:05:36 +0100

    Added footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,10 @@
 <template>
-  <footer class="bg-teal-300 w-full h-12 xl:mt-20 2xl:mt-48 py-auto text-center">
+  <footer
+    class="
+    bg-teal-300 text-gray-900 w-1/2 h-8 xl:mt-20
+    2xl:mt-48 py-auto text-center mx-auto rounded-t
+    "
+  >
     {{ (new Date()).getFullYear() }} &copy; Jeroen Claessens
   </footer>
 </template>
