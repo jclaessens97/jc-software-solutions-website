640fb5676353f66433b417e22825443c7a7de06d
Jeroen Claessens <jclaessens97@hotmail.com>
2021-01-30 10:24:52 +0100

    Added noopener noreferrer

--- components/Socials.vue
+++ components/Socials.vue
@@ -3,16 +3,16 @@
     class="text-3xl text-white fixed bottom-0 h-auto z-10
     flex flex-col space-y-3 mb-8 ml-5 justify-center"
   >
-    <a href="https://www.linkedin.com/in/jclaessens" target="_blank">
+    <a href="https://www.linkedin.com/in/jclaessens" target="_blank" rel="noreferrer noopener">
       <font-awesome-icon :icon="['fab', 'linkedin-in']" />
     </a>
-    <a href="https://github.com/jclaessens97" target="_blank">
+    <a href="https://github.com/jclaessens97" target="_blank" rel="noreferrer noopener">
       <font-awesome-icon :icon="['fab', 'github']" />
     </a>
-    <a href="https://discordapp.com/users/310137027330965514/" target="_blank">
+    <a href="https://discordapp.com/users/310137027330965514/" target="_blank" rel="noreferrer noopener">
       <font-awesome-icon :icon="['fab', 'discord']" />
     </a>
-    <a href="mailto:jclaessens97@hotmail.com">
+    <a href="mailto:jclaessens97@hotmail.com" rel="noreferrer noopener">
       <font-awesome-icon :icon="['far', 'envelope']" />
     </a>
   </aside>
