98a28784a1feda6aec762007f5165c2c678d2265
Jeroen Claessens <jclaessens97@hotmail.com>
2021-02-04 12:03:51 +0100

    i18n error messages

--- components/Contact.vue
+++ components/Contact.vue
@@ -22,7 +22,7 @@
           v-for="(error, index) in errorMessages"
           :key="`error-${index}`"
         >
-          {{ error }}
+          {{ $t(error) }}
         </li>
       </ul>
       <div class="flex flex-wrap -mx-3 mb-6">
@@ -141,14 +141,14 @@ export default {
         && (!this.input.email || this.input.email.length === 0)
         && (!this.input.message || this.input.message.length === 0)
       ) {
-        this.errorMessages.push('Please fill in all required fields.');
+        this.errorMessages.push('error.required');
         invalid = true;
       }
 
       // eslint-disable-next-line
       const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
       if (!emailRegex.test(this.input.email)) {
-        this.errorMessages.push('Please fill in a valid email address');
+        this.errorMessages.push('error.email');
         invalid = true;
       }
 
@@ -172,7 +172,7 @@ export default {
           });
           this.success = true;
         } catch (err) {
-          this.errorMessages.push('Something went wrong. Please try again later.');
+          this.errorMessages.push('error.unknown');
           this.invalid = true;
         }
 
