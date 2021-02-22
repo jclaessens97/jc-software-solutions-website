3210d5abe31dc3c929d93d1d8355f58985577d47
Jeroen Claessens <jclaessens97@hotmail.com>
2021-02-22 14:46:54 +0100

    Updated favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,10 @@ export default {
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [
-      // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' },
+      { rel: 'icon', sizes: '32x32', href: '/favicons/favicon-32x32.png' },
+      { rel: 'icon', sizes: '16x16', href: '/favicons/favicon-16x16.png' },
+      { rel: 'manifest', href: '/favicons/site.webmanifest' },
     ],
   },
 
